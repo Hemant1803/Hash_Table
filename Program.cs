9f20bf5e322081349a1ac5b6bf76da6e61e1a187
Hemant1803 <hshemantsharma445@gmail.com>
2023-10-06 02:31:28 +0530

    Hash Table UC1,UC2,UC3

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+namespace Hash_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "“Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations”";
+            string wordToRemove = "avoidable";
+            string[] words = sentence.Split(' ');
+
+            UC1_2_3 counter = new UC1_2_3(10); // Choose an appropriate bucket count
+
+            foreach (var word in words)
+            {
+                counter.AddWord(word);
+            }
+
+            Console.WriteLine("Word Frequencies:");
+            Console.WriteLine(sentence);
+            foreach (var word in words)
+            {
+               
+                int frequency = counter.GetWordFrequency(word);
+                Console.WriteLine($"{word}: {frequency}");
+            }
+           Console.WriteLine("**************************************");
+            counter.RemoveWord(wordToRemove);
+
+            Console.WriteLine("Phrase after removing the word:");
+            foreach (var word in words)
+            {
+                if (word != wordToRemove)
+                {
+                    Console.Write(word + " ");
+                }
+            }
+            Console.WriteLine();
+        
+    }
+    }
+}
---
+++ UC1_2_3.cs
@@ -0,0 +1,80 @@
+﻿namespace Hash_Table
+{
+     class UC1_2_3
+    {
+        private readonly int bucketCount;
+        private readonly LinkedList<MyMapNode<string, int>>[] hashTable;
+
+        public UC1_2_3(int bucketCount)
+        {
+            this.bucketCount = bucketCount;
+            hashTable = new LinkedList<MyMapNode<string, int>>[bucketCount];
+            for (int i = 0; i < bucketCount; i++)
+            {
+                hashTable[i] = new LinkedList<MyMapNode<string, int>>();
+            }
+        }       
+        private int GetBucketIndex(string word)
+        {
+            int hash = word.GetHashCode();
+            return Math.Abs(hash) % bucketCount;
+        }
+
+        
+        public void AddWord(string word)
+        {
+            int bucketIndex = GetBucketIndex(word);
+
+            foreach (var node in hashTable[bucketIndex])
+            {
+                if (node.Key == word)
+                {
+                    node.Value++;
+                    return;
+                }
+            }
+
+            var newNode = new MyMapNode<string, int>(word, 1);
+            hashTable[bucketIndex].AddLast(newNode);
+        }
+        public void RemoveWord(string word)
+        {
+            int bucketIndex = GetBucketIndex(word);
+
+            LinkedListNode<MyMapNode<string, int>> nodeToRemove = null;
+
+            foreach (var node in hashTable[bucketIndex])
+            {
+                if (node.Key == word)
+                {
+                   
+                    break;
+                }
+            }
+
+            if (nodeToRemove != null)
+            {
+                hashTable[bucketIndex].Remove(nodeToRemove);
+            }
+        }
+
+
+      
+        public int GetWordFrequency(string word)
+        {
+            int bucketIndex = GetBucketIndex(word);
+
+            foreach (var node in hashTable[bucketIndex])
+            {
+                if (node.Key == word)
+                {
+                    return node.Value;
+                }
+            }
+
+
+            // Word not found in the hash table
+            return 0;
+        }
+    }
+}
